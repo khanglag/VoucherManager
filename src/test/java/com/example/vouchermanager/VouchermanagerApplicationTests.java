e9fe25b0715892901ef202cfd26493d6c1c37862
Minh Khang <khangminh.do2003@gmail.com>
2025-03-09 00:20:42 +0700

    t

---
+++ src/test/java/com/example/vouchermanager/VouchermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vouchermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VouchermanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
